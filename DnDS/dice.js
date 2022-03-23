6b28c0a566396bf968db495bcb578e341f247681
Daniel Carr <dbcarr980@gmail.com>
2022-03-23 17:09:51 -0600

    dice

---
+++ DnDS/dice.js
@@ -0,0 +1,56 @@
+document.getElementById('d4').addEventListener("click", r4);
+function r4() {
+
+    result = Math.floor(Math.random() * 4) + 1;
+    document.getElementById("r4").innerHTML = result;
+    console.log('end of search');
+}
+
+document.getElementById('d6').addEventListener("click", r6);
+function r6() {
+
+    result = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("r6").innerHTML = result;
+    console.log('end of search');
+}
+
+document.getElementById('d8').addEventListener("click", r8);
+function r8() {
+
+    result = Math.floor(Math.random() * 8) + 1;
+    document.getElementById("r8").innerHTML = result;
+    console.log('end of search');
+}
+
+document.getElementById('d10').addEventListener("click", r10);
+function r10() {
+
+    result = Math.floor(Math.random() * 10) + 1;
+    document.getElementById("r10").innerHTML = result;
+    console.log('end of search');
+}
+
+document.getElementById('d12').addEventListener("click", r12);
+function r12() {
+
+    result = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("r12").innerHTML = result;
+    console.log('end of search');
+}
+
+document.getElementById('d20').addEventListener("click", r20);
+function r20() {
+
+    result = Math.floor(Math.random() * 20) + 1;
+    document.getElementById("r20").innerHTML = result;
+    console.log('end of search');
+}
+
+document.getElementById('d100').addEventListener("click", r100);
+function r100() {
+
+    result = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("r100").innerHTML = result;
+    console.log('end of search');
+}
+
